e50b723efdb9000d204e2d2c4f69478a58ba0d98
Ethan Vernon <ethan.vernon@colorado.edu>
2019-08-04 18:48:02 -0500

    nesting components

--- main.js
+++ main.js
@@ -10,6 +10,26 @@ Vue.component('practice-component', {
 
 });
 
+Vue.component('practice-wrapper', {
+
+	template: `
+		<div>
+			<practice-component v-for="task in tasks">{{task.description}}</practice-component>
+		</div>
+	`,
+	data() {
+		return {
+			tasks: [
+				{description: 'get air mattress', completed: 'yes'},
+				{description: 'get food', completed: 'yes'},
+				{description: 'clean apartment', completed: 'no'},
+				{description: 'get furniture', completed: 'no'},				
+			]
+		}
+	}
+
+})
+
 
 new Vue({
 	el: '#component'
