605fd7e9d348c4e0bc6e514c457ba3214a590f42
Ethan Vernon <ethan.vernon@colorado.edu>
2019-08-04 19:05:22 -0500

    recreating a bulma element as a component

--- main.js
+++ main.js
@@ -30,6 +30,39 @@ Vue.component('practice-wrapper', {
 
 })
 
+Vue.component('message', {
+
+	props: ['title', 'body'],
+
+	data() {
+		return {
+			isVisible: true
+		}
+	},
+
+	template: `
+
+		<article class="message" v-show='isVisible'>
+		  <div class="message-header">
+		    {{ title }}
+		    <button class='close' @click='hideModal'>x</button>
+		  </div>
+		  <div class="message-body">
+		  	{{ body }}
+		  </div>
+		</article>
+
+	`,
+
+	methods: {
+
+		hideModal() {
+			this.isVisible = false;
+		}
+	}
+
+})
+
 
 new Vue({
 	el: '#component'
